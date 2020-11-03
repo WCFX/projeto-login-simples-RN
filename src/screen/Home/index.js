5e749d384e9830ec8193ad57c0ec8e08117bfb9d
wcfx <wcfx.dev@gmail.com>
2020-11-03 01:26:22 -0300

    API concluida. Att Feita com sucesso

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,14 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 import styles from './style';
 
 
 const Home = () => {
 
+  const [loading, setLoading ] = useState(false);
+
+  const [email, setEmail ] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [status, setStatus ] = useState('');
+
+  async function handleVerifyLogin(){
+    setStatus('');
+    setLoading(true);
+    const req = await fetch('https://api.b7web.com.br/loginsimples/', {
+      method: 'POST',
+      body: JSON.stringify({email,password}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const json = await req.json();
+
+    if(json.status == 'ok'){
+      setStatus('Login Efetuado com sucesso !')
+    }else {
+      setStatus('Os dados de Email e/ou senha estão incorretos!')
+    }
+    setLoading(false);
+  }
+
   return(
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Olá Mundo de Merda !</Text>
+
+      <View style={styles.areaLogin}>
+        <Text style={styles.title}>Faça o seu login</Text>  
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Digite seu Email"
+          placeholderTextColor="#bbb"
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+          placeholder="Digite sua Senha"
+          placeholderTextColor="#bbb"
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleVerifyLogin}>
+          <Text style={styles.buttonText}>Logar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {loading && 
+        <View>
+          <ActivityIndicator color="#2ED573" size="large"/>
+          <Text style={styles.activityIndicatorText}>Loading ...</Text>
+        </View>
+      }
+
+      {!loading &&
+        <View style={styles.containerMsg}>
+          <Text style={styles.msg}>{status}</Text>
+        </View>
+      }
+
+
     </SafeAreaView>
   )
 };
--- src/screen/Home/style.js
+++ src/screen/Home/style.js
@@ -4,13 +4,57 @@ import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#eee'
+    backgroundColor: '#eee',
+    width: '100%',
+  },
+  areaLogin:{
+    marginVertical: 40,
+    width: '75%',
   },
   title: {
+    fontSize: 24,
+    fontFamily: 'Nunito_700Bold',
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  input:{
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 8,
+    fontSize: 18,
+    fontFamily: 'Archivo_400Regular',
+  },
+  button: {
+    borderWidth: 1,
+    borderRadius: 3,
+    height: 40,
+    width: '70%',
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5ea676',
+
+  },
+  buttonText:{
+    fontSize: 20,
+    letterSpacing: 1,
+    color: '#fff',
+    fontFamily: 'Poppins_400Regular',
+  },
+  containerMsg: {
+    marginTop: 50,
+    alignSelf: 'center',
+    maxWidth: 300,
+    textAlign: 'center',
+  },
+  msg: {
     fontSize: 22,
   },
+  activityIndicatorText: {
+    fontSize: 22,
+  }
   
 });
 
